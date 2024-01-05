f60bbf06f1890921c44774e64c4dcc729690a189
Stephanie Charriere <stephaniescharriere@gmail.com>
2024-01-05 11:08:52 -0600

    Chapter 2 part 1
    First half of video up to 36:50

---
+++ chapter_2/sandbox.js
@@ -0,0 +1,62 @@
+// alert("hello, world");
+// console.log("1");
+// console.log("2");
+
+/*
+let age = 25;
+let year = 2024
+console.log(age, year);
+
+age = 30;
+console.log(age, year);
+
+const points = 100;
+// points = 50 // errors because cant over write const value
+console.log(points)
+
+var score = 75; // older way but still works
+console.log(score)
+*/
+
+// // strings
+// console.log("hello, world");
+
+// let email = "mario@email.com";
+// console.log(email);
+
+// // string concatenation
+// let firstName = "Brent";
+// let lastName = "Sanderson";
+
+// let fullName = firstName + " " + lastName;
+
+// console.log(fullName);
+
+// // getting characters
+// console.log(fullName[0]);
+
+// // string length
+// console.log(fullName.length); // note the space is included as a char to the length
+
+// // string methods (same as function)
+// // to Upper is a method so needs a ()
+// console.log(fullName.toLocaleUpperCase());
+// let result = fullName.toLocaleLowerCase();
+// console.log(result)
+
+// let index = email.indexOf("@"); // store the place of that symbol in a number
+// console.log(index);
+
+// common string methods
+let email = 'mario@gmail.com';
+
+// let result = email.lastIndexOf('m');
+
+//  let result = email.slice(0,10) // slice it out of the string, from 0 to 10 (or from 3 to 10)
+
+// let result = email.substr(4,10) // will take starting at 4 and next 10
+
+// let result = email.replace("m", "w"); // only replaces first instanve not all
+// let result = email.replace(/m/g, "w"); // replaces all (g means global)
+
+console.log(result);
